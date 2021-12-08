289853cfed6730910a795f0f0672665ed3bb119f
Julien VON DER MARCK <[julien.von-der-marck]@edu.unistra.fr>
2021-12-08 22:30:41 +0100

    bug ajout fichier manquant au commit

---
+++ src/model/set/number/Quatro.java
@@ -0,0 +1,9 @@
+package model.set.number;
+import model.set.NumberPlayer;
+
+public class Quatro {
+    public void setNumberPlayers(NumberPlayer player)
+    {
+        player.setNumberPlayers(4);
+    }
+}
