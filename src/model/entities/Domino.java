f09266c21092b1584e1c550bf37a5011efdf160b
Alexander Yanovskyy <yanovskyy@unistra.fr>
2021-12-08 19:53:45 +0100

    Domino complet, Graph en cours de travaille

--- src/model/entities/Domino.java
+++ src/model/entities/Domino.java
@@ -18,7 +18,26 @@ public class Domino {
         _arrayTile[0][1] = tile2;
     }
 
-    public void rotate(){
+    public boolean isXX(){
+        return this.getTile()[1][0] != null;
+    }
 
+    public boolean isXY(){
+        return this.getTile()[0][1] != null;
+    }
+
+    public Tile[][] getTile(){
+        return _arrayTile;
+    }
+
+    public void rotate(){
+        if(this.isXX()){
+            _arrayTile[0][1] = _arrayTile[1][0];
+            _arrayTile[1][0] = null;
+        }
+        else{ // si this.XY == true
+            _arrayTile[1][0] = _arrayTile[0][1];
+            _arrayTile[0][1] = null;
+        }
     }
 }
--- src/model/entities/Graph.java
+++ src/model/entities/Graph.java
@@ -1,4 +1,37 @@
 package model.entities;
 
 public class Graph {
+    private Domino[][] _arrayDominoes;
+    private Tile[][] _arrayTiles;
+
+    public Graph(){
+        _arrayDominoes = new Domino[5][5];
+        _arrayTiles = new Tile[5][5];
+    }
+
+    public Domino getDomino(int x, int y){
+        return _arrayDominoes[x][y];
+    }
+
+    public void setDomino(Domino domino, int x, int y){
+        _arrayTiles[x][y] = domino.getTile()[0][0];
+
+        //Si le domino est comme ça :
+        // O O
+        // X X
+        if(domino.isXX()){// pas videvide
+            _arrayTiles[x+1][y] = domino.getTile()[0][1];
+        }
+        //Si le domino est comme ça :
+        // O X
+        // O X
+        else if(domino.isXY()){
+            _arrayTiles[x][y + 1] = domino.getTile()[1][0];
+        }
+
+    }
+
+    public boolean isPlaceAvailable(int x, int y){
+        return false;
+    }
 }
