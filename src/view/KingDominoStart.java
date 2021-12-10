cedf445bcdc337b8e61d6fc41ce607ff9141a3bc
Julien VON DER MARCK <[julien.von-der-marck]@edu.unistra.fr>
2021-12-10 21:37:20 +0100

    KingDominoStart.java : placement des élements sur la fenetre en ayant ajoute des contraintes

--- src/view/KingDominoStart.java
+++ src/view/KingDominoStart.java
@@ -12,11 +12,14 @@ import static view.Window.*;
 public class KingDominoStart implements Observer{
 
     private final String[] strategyPlayers;
+    private final String[] gameMode;
     private final JComboBox _cboStrategys;
+    private final JComboBox _cboGameMode;
     private JLabel _labelTitle;
     private JPanel _panelMain;
-    private JPanel _panelTitle;
+    private JPanel _panelComboBox;
     private JPanel _panelSet;
+    private JButton _btnValidate;
     private static KingDominoStart instance;
     private Window _window;
 
@@ -25,28 +28,48 @@ public class KingDominoStart implements Observer{
         _window = Window.instance; // we get our main window to access to its variables
         _window.frame.setTitle("Initialisation of the KingDomino");
 
+        // Constraints
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
         // MAIN PANEL : Panel where we'll find all panels and elements
         _panelMain = new JPanel();
-        _panelMain.setLayout( new BoxLayout(_panelMain, BoxLayout.Y_AXIS) );
+        _panelMain.setLayout( new GridBagLayout() );
 
-        // TITLE AND INFO PANEL : Panel to display title and information about what to do
-        _panelTitle = new JPanel();
-        _panelTitle.setLayout( new BorderLayout() );
-        _labelTitle =  new JLabel();
-        _labelTitle.setText("Bienvenue sur KingDomino");
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.weightx = 2;
+        constraints.weighty = 2;
+        constraints.insets = new Insets(0,0,0,0); //top padding
+        _labelTitle =  new JLabel("Bienvenue sur KingDomino", SwingConstants.CENTER);
+        _labelTitle.setFont(new Font("Century Gothic", Font.PLAIN, 25));
         _labelTitle.setVisible(true);
-        _panelTitle.add(_labelTitle,  BorderLayout.NORTH);
+        _panelMain.add(_labelTitle, constraints);
 
-        // SET PANEL : a panel to show all elements to start a game, numbers of players and game mode
-        _panelSet = new JPanel();
-        _panelSet.setLayout( new GridLayout(3,1) );
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.weightx = 0.1;
+        constraints.weighty = 0.1;
         strategyPlayers = new String[]{"Duo", "Trio", "Quatro"};
         _cboStrategys = new JComboBox(strategyPlayers);
-        _panelSet.add(_cboStrategys);
+        _cboStrategys.setMaximumSize(new Dimension(10,10));
+        _panelMain.add(_cboStrategys, constraints);
+
+        constraints.gridx = 1;
+        gameMode = new String[]{"Harmony", "The middle Kingdom"};
+        _cboGameMode = new JComboBox(gameMode);
+        _panelMain.add(_cboGameMode, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        constraints.insets = new Insets(100,0,10,0); //top padding
+        _btnValidate = new JButton("Validate your choices");
+        _panelMain.add(_btnValidate, constraints);
 
         // MAIN PANEL : We put element in the main Panel
-        _panelMain.add(_panelTitle);
-        _panelMain.add(_panelSet);
         _window.frame.setContentPane( _panelMain);
         _window.frame.setVisible( true );
 
