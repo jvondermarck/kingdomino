6f5658a93fac476eec3c20a9c2db9243abe6c42e
Julien VON DER MARCK <[julien.von-der-marck]@edu.unistra.fr>
2021-12-11 14:51:45 +0100

    KingDominoStart.java : design fonctionnel et beau realisé grace a une maquette faite sur Figma

--- src/view/KingDominoStart.java
+++ src/view/KingDominoStart.java
@@ -18,20 +18,24 @@ public class KingDominoStart implements Observer{
     private final JComboBox _cboStrategys;
     private final JRadioButton _rdbGameModeHarmony;
     private final JRadioButton _rdbGameModeMiddle;
+    private final JRadioButton _rdbGameNothing;
     private JLabel _labelTitle;
+    private JLabel _labelTitlePlayer;
+    private JLabel _labelTitleMode;
     private JPanel _panelMain;
     private JButton _btnValidate;
     private static KingDominoStart instance;
     private Window _window;
     private Font _fontGermania;
     private Font _fontTimeless;
+    private DefaultListCellRenderer listRenderer;
 
     private KingDominoStart() {
         // SET UP THE WINDOW
         _window = Window.instance; // we get our main window to access to its variables
         _window.frame.setTitle("Initialisation of the KingDomino");
         _window.frame.setResizable(false);
-        final ImageIcon icon = new ImageIcon("img/kingdomino1.png");
+        final ImageIcon icon = new ImageIcon("img/LaunchScreen.png");
         _fontGermania = FontUtilities.setFont("font/Germania.otf");
         _fontTimeless = FontUtilities.setFont("font/Timeless.ttf");
 
@@ -47,50 +51,89 @@ public class KingDominoStart implements Observer{
             {setOpaque(false);}
             public void paintComponent(Graphics graphics)
             {
-                graphics.drawImage(img, 0, 0, this);
+                graphics.drawImage(img, 0, -35, this);
                 super.paintComponent(graphics);
             }
         };
         _panelMain.setLayout( new GridBagLayout() );
 
+        // LABEL TITLE WELCOME
         constraints.gridx = 0;
         constraints.gridy = 0;
         constraints.gridwidth = 3;
-        constraints.weighty = 2;
-        constraints.insets = new Insets(0,0,0,30); //top padding
-        _labelTitle =  new JLabel("Welcome to KingDomino", SwingConstants.CENTER);
+        constraints.weighty = 3;
+        constraints.insets = new Insets(0,0,10,30); //top padding
+        _labelTitle =  new JLabel("Welcome to Kingdomino", SwingConstants.CENTER);
         _labelTitle.setFont(_fontGermania.deriveFont(Font.PLAIN, 40));
         _labelTitle.setVisible(true);
         _panelMain.add(_labelTitle, constraints);
 
+        // LABEL TITLE CHOOSE PLAYERS
         constraints.gridx = 0;
         constraints.gridy = 1;
         constraints.weighty = 0.1;
-        constraints.gridwidth = 1;
-        constraints.insets = new Insets(0,50,0,200);
+        constraints.gridwidth = 3;
+        constraints.insets = new Insets(0,100,10,50);
+        _labelTitlePlayer = new JLabel("Choose the amount of players");
+        _labelTitlePlayer.setFont(_fontGermania.deriveFont(Font.PLAIN, 27));
+        _panelMain.add(_labelTitlePlayer, constraints);
+
+        // COMBOBOX NUMBER OF PLAYERS
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.weighty = 0.1;
+        constraints.gridwidth = 3;
+        constraints.insets = new Insets(0,200,30,200);
         strategyPlayers = new String[]{"Duo", "Trio", "Quatro"};
         _cboStrategys = new JComboBox(strategyPlayers);
         _cboStrategys.setFont(_fontTimeless.deriveFont(Font.PLAIN, 15));
-        _cboStrategys.setMaximumSize(new Dimension(10,10));
+        listRenderer = new DefaultListCellRenderer();
+        listRenderer.setHorizontalAlignment(DefaultListCellRenderer.CENTER); // center-aligned items
+        _cboStrategys.setRenderer(listRenderer);
         _panelMain.add(_cboStrategys, constraints);
 
-        constraints.insets = new Insets(0,100,0,0);
+        // TITLE CHOOSE GAME MODE
+        constraints.insets = new Insets(0,150,10,0);
         constraints.gridx = 0;
-        constraints.gridy = 2;
+        constraints.gridy = 3;
+        constraints.weighty = 0.1;
+        constraints.gridwidth = 3;
+        _labelTitleMode = new JLabel("Choose game mode");
+        _labelTitleMode.setFont(_fontGermania.deriveFont(Font.PLAIN, 27));
+        _panelMain.add(_labelTitleMode, constraints);
+
+        constraints.insets = new Insets(0,70,60,0);
+        constraints.gridx = 0;
+        constraints.gridy = 4;
         constraints.gridwidth = 1;
+        _rdbGameNothing = new JRadioButton("Nothing");
+        _rdbGameNothing.setFont(_fontTimeless.deriveFont(Font.PLAIN, 15));
+        _rdbGameNothing.setOpaque(false);
+        _panelMain.add(_rdbGameNothing, constraints);
+
+        constraints.insets = new Insets(0,15,60,0);
+        constraints.gridx = 1;
         _rdbGameModeHarmony = new JRadioButton("Harmony");
+        _rdbGameModeHarmony.setFont(_fontTimeless.deriveFont(Font.PLAIN, 15));
+        _rdbGameModeHarmony.setOpaque(false);
         _panelMain.add(_rdbGameModeHarmony, constraints);
 
-        constraints.gridx = 1;
-        constraints.insets = new Insets(0,0,0,100);
+        constraints.gridx = 2;
+        constraints.insets = new Insets(0,15,60,0);
         _rdbGameModeMiddle = new JRadioButton("The middle Kingdom");
+        _rdbGameModeMiddle.setFont(_fontTimeless.deriveFont(Font.PLAIN, 15));
+        _rdbGameModeMiddle.setOpaque(false);
         _panelMain.add(_rdbGameModeMiddle, constraints);
 
         constraints.gridx = 0;
-        constraints.gridy = 3;
-        constraints.gridwidth = 2;
-        constraints.insets = new Insets(100,150,10,150);
-        _btnValidate = new JButton("Validate your choices");
+        constraints.gridy = 5;
+        constraints.gridwidth = 3;
+        constraints.insets = new Insets(0,165,10,150);
+        _btnValidate = new JButton("Start");
+        _btnValidate.setFont(_fontGermania.deriveFont(Font.PLAIN, 30));
+        _btnValidate.setOpaque(false);
+        _btnValidate.setContentAreaFilled(false);
+        _btnValidate.setBorderPainted(false);
         _panelMain.add(_btnValidate, constraints);
 
         // MAIN PANEL : We put element in the main Panel
@@ -114,6 +157,20 @@ public class KingDominoStart implements Observer{
                     _window._controller.switchToQuatro();
 
                 _window._game.addObservers(new KingDominoGame());
+            }
+        });
+
+        _rdbGameNothing.addActionListener( new ActionListener() {
+            @Override
+            public void actionPerformed( ActionEvent actionEvent ) {
+                if(_rdbGameNothing.isSelected())
+                {
+                    _rdbGameModeHarmony.setEnabled(false);
+                    _rdbGameModeMiddle.setEnabled(false);
+                } else {
+                    _rdbGameModeHarmony.setEnabled(true);
+                    _rdbGameModeMiddle.setEnabled(true);
+                }
             }
         });
 
